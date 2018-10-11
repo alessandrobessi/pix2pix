024d153a156b5097b87005d0c198c1cdc3515149
Alessandro Bessi <alessandro.bessi@mail.com>
2018-10-11 14:38:21 +0200

    Implement epochs

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from pix2pix.discriminator import Discriminator
 from pix2pix.losses import generator_loss, discriminator_loss, loss
 
 if __name__ == '__main__':
+
+    num_epochs = 1
+
     train_dataset = FacadesDataset(root_dir=os.path.join(os.getcwd(), 'data'),
                                    split='train',
                                    transform=Transform())
@@ -38,42 +41,44 @@ if __name__ == '__main__':
     train_loss = 0
     val_loss = 0
 
-    for input_img, real_img in tqdm(train_loader):
-        g_optim.zero_grad()
-        d_optim.zero_grad()
+    for epoch in range(num_epochs):
+        for input_img, real_img in tqdm(train_loader, desc='Epoch {}'.format(epoch)):
+            g_optim.zero_grad()
+            d_optim.zero_grad()
 
-        generated_img = generator(input_img)
-        d_judge_generated = discriminator(generated_img, real_img)
-        d_judge_real = discriminator(input_img, real_img)
-
-        g_loss = generator_loss(d_judge_generated, generated_img, real_img)
-        d_loss = discriminator_loss(d_judge_real, d_judge_generated)
-        train_loss = loss(g_loss, d_loss)
-        train_loss.backward()
-
-        g_optim.step()
-        d_optim.step()
-
-        print("Train Loss: {}".format(float(train_loss.data)))
-
-    with torch.no_grad():
-
-        for i, input_img, real_img in enumerate(tqdm(val_loader)):
             generated_img = generator(input_img)
             d_judge_generated = discriminator(generated_img, real_img)
             d_judge_real = discriminator(input_img, real_img)
 
             g_loss = generator_loss(d_judge_generated, generated_img, real_img)
             d_loss = discriminator_loss(d_judge_real, d_judge_generated)
-            val_loss = loss(g_loss, d_loss)
-            print("Val Loss: {}".format(float(val_loss.data)))
+            train_loss = loss(g_loss, d_loss)
+            train_loss.backward()
 
-            if i % 10:
-                plt.imshow(input_img, interpolation='nearest')
-                plt.show()
+            g_optim.step()
+            d_optim.step()
 
-                plt.imshow(real_img, interpolation='nearest')
-                plt.show()
+            print("Train Loss: {}".format(float(train_loss.data)))
 
-                plt.imshow(generated_img, interpolation='nearest')
-                plt.show()
+        with torch.no_grad():
+
+            for i, input_img, real_img in enumerate(tqdm(val_loader,
+                                                         desc='Epoch {}'.format(epoch))):
+                generated_img = generator(input_img)
+                d_judge_generated = discriminator(generated_img, real_img)
+                d_judge_real = discriminator(input_img, real_img)
+
+                g_loss = generator_loss(d_judge_generated, generated_img, real_img)
+                d_loss = discriminator_loss(d_judge_real, d_judge_generated)
+                val_loss = loss(g_loss, d_loss)
+                print("Val Loss: {}".format(float(val_loss.data)))
+
+                if i % 10:
+                    plt.imshow(input_img, interpolation='nearest')
+                    plt.show()
+
+                    plt.imshow(real_img, interpolation='nearest')
+                    plt.show()
+
+                    plt.imshow(generated_img, interpolation='nearest')
+                    plt.show()
