24c117f0ec29a417a438849aa16544f3337200b5
Alessandro Bessi <alessandro.bessi@mail.com>
2018-10-14 06:25:42 +0200

    remove comments, add gan+l1 loss logging

--- main.py
+++ main.py
@@ -42,9 +42,6 @@ if __name__ == '__main__':
     g_optim = torch.optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
     d_optim = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=(0.5, 0.999))
 
-    # g_scheduler = torch.optim.lr_scheduler.StepLR(g_optim, step_size=num_epochs // 20, gamma=0.8)
-    # d_scheduler = torch.optim.lr_scheduler.StepLR(d_optim, step_size=num_epochs // 20, gamma=0.8)
-
     writer = tensorboardX.SummaryWriter(logs_dir)
 
     step = 0
@@ -75,6 +72,7 @@ if __name__ == '__main__':
 
             writer.add_scalar('generator_loss_l1', float(g_loss_l1.data), step)
             writer.add_scalar('generator_loss_gan', float(g_loss_gan.data), step)
+            writer.add_scalar('generator_loss_gan+l1', float(g_loss_gan.data), step)
             writer.add_scalar('discriminator_loss', float(d_loss.data), step)
 
         with torch.no_grad():
@@ -89,9 +87,6 @@ if __name__ == '__main__':
                 val_loss = loss(g_loss_l1, g_loss_gan, d_loss)
 
         writer.add_scalar('val_loss', float(val_loss.data), step)
-
-        # writer.add_scalar('lr_generator', g_scheduler.get_lr()[0], step)
-        # writer.add_scalar('lr_discriminator', d_scheduler.get_lr()[0], step)
 
         save_image([input_img, real_img, generated_img], examples_dir, epoch)
 
